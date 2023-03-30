be1be61dd25966a91b421026ba06f9f69f6a2641
brarie <52105387+brarie@users.noreply.github.com>
2023-03-30 19:12:30 +0900

    even_list finsished

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from typing import List
 def even_list(int_list: List[int]) -> List[int]:
     """
     Determines if a number is even and return an even list.
-    
+
     Args:
         int_list: A list of integer.
 
@@ -12,7 +12,7 @@ def even_list(int_list: List[int]) -> List[int]:
         A list of even integers.
     """
     # TODO: Implement even_list
-    pass
+    return [x for x in int_list if x % 2 == 0]
 
 # Skeleton code for sum_of_squares_of_even
 def sum_of_squares_of_even(even_int_list: List[int]) -> int:
